005a7bae0d1e1b9b8f375bf6e44bdb2ce5199cbf
Unknown <bartstarss@gmail.com>
2019-12-17 15:05:44 +0100

    creating FreeRTOS SDK

---
+++ MiniZED_VIO_HC_SR04/MiniZED_AXI_HC_SR04.sdk/eos_opdracht/Debug/src/freertos_hello_world.d
@@ -0,0 +1,83 @@
+src/freertos_hello_world.o src/freertos_hello_world.o: \
+ ../src/freertos_hello_world.c \
+ ../../eos_opdracht_bsp/ps7_cortexa9_0/include/xgpio.h \
+ ../../eos_opdracht_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../eos_opdracht_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../eos_opdracht_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../eos_opdracht_bsp/ps7_cortexa9_0/include/xgpio_l.h \
+ ../../eos_opdracht_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../eos_opdracht_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../eos_opdracht_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../eos_opdracht_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../eos_opdracht_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../eos_opdracht_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../eos_opdracht_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../eos_opdracht_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../eos_opdracht_bsp/ps7_cortexa9_0/include/FreeRTOS.h \
+ ../../eos_opdracht_bsp/ps7_cortexa9_0/include/FreeRTOSConfig.h \
+ ../../eos_opdracht_bsp/ps7_cortexa9_0/include/projdefs.h \
+ ../../eos_opdracht_bsp/ps7_cortexa9_0/include/portable.h \
+ ../../eos_opdracht_bsp/ps7_cortexa9_0/include/deprecated_definitions.h \
+ ../../eos_opdracht_bsp/ps7_cortexa9_0/include/portmacro.h \
+ ../../eos_opdracht_bsp/ps7_cortexa9_0/include/mpu_wrappers.h \
+ ../../eos_opdracht_bsp/ps7_cortexa9_0/include/task.h \
+ ../../eos_opdracht_bsp/ps7_cortexa9_0/include/list.h \
+ ../../eos_opdracht_bsp/ps7_cortexa9_0/include/queue.h \
+ ../../eos_opdracht_bsp/ps7_cortexa9_0/include/task.h \
+ ../../eos_opdracht_bsp/ps7_cortexa9_0/include/timers.h \
+ ../../eos_opdracht_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../eos_opdracht_bsp/ps7_cortexa9_0/include/xparameters.h
+
+../../eos_opdracht_bsp/ps7_cortexa9_0/include/xgpio.h:
+
+../../eos_opdracht_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../eos_opdracht_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../eos_opdracht_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../eos_opdracht_bsp/ps7_cortexa9_0/include/xgpio_l.h:
+
+../../eos_opdracht_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../eos_opdracht_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../eos_opdracht_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../eos_opdracht_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../eos_opdracht_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../eos_opdracht_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../eos_opdracht_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../eos_opdracht_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../eos_opdracht_bsp/ps7_cortexa9_0/include/FreeRTOS.h:
+
+../../eos_opdracht_bsp/ps7_cortexa9_0/include/FreeRTOSConfig.h:
+
+../../eos_opdracht_bsp/ps7_cortexa9_0/include/projdefs.h:
+
+../../eos_opdracht_bsp/ps7_cortexa9_0/include/portable.h:
+
+../../eos_opdracht_bsp/ps7_cortexa9_0/include/deprecated_definitions.h:
+
+../../eos_opdracht_bsp/ps7_cortexa9_0/include/portmacro.h:
+
+../../eos_opdracht_bsp/ps7_cortexa9_0/include/mpu_wrappers.h:
+
+../../eos_opdracht_bsp/ps7_cortexa9_0/include/task.h:
+
+../../eos_opdracht_bsp/ps7_cortexa9_0/include/list.h:
+
+../../eos_opdracht_bsp/ps7_cortexa9_0/include/queue.h:
+
+../../eos_opdracht_bsp/ps7_cortexa9_0/include/task.h:
+
+../../eos_opdracht_bsp/ps7_cortexa9_0/include/timers.h:
+
+../../eos_opdracht_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../eos_opdracht_bsp/ps7_cortexa9_0/include/xparameters.h:
